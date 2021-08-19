e99f48793314db28f3eec495cd895b7cf7c94d77
김현지 <guswlhjk@naver.com>
2021-08-19 15:15:13 +0900

    다른 화면의 UI 작업

--- app/src/main/java/com/nepplus/intent/MainActivity.kt
+++ app/src/main/java/com/nepplus/intent/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.nepplus.intent
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        moveToOtherBtn.setOnClickListener {
+            //버튼을 눌리면 => OtherActivity로 이동
+            //Main -> Other
+
+            val myIntent = Intent( this, OtherActivity2 :: class.java )
+            startActivity(myIntent)
+
+
+        }
     }
 }
