80f5e7b4f0955ba0596c6d06e8eb80cc5eee3240
김현지 <guswlhjk@naver.com>
2021-08-19 15:03:22 +0900

    Initial commit

---
+++ app/src/main/java/com/nepplus/intent/OtherActivity2.kt
@@ -0,0 +1,11 @@
+package com.nepplus.intent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OtherActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_other2)
+    }
+}
